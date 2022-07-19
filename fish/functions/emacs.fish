fdb8cfe960688ad07a6eb6d30b45cdbfb0f0f6c9
Vedang Manerikar <ved.manerikar@gmail.com>
2022-07-19 13:58:08 -0700

    Add Emacs.App to the path

---
+++ fish/functions/emacs.fish
@@ -0,0 +1,3 @@
+function emacs --wraps='/Applications/Emacs.app/Contents/MacOS/Emacs -nw' --description 'alias emacs=/Applications/Emacs.app/Contents/MacOS/Emacs -nw'
+  /Applications/Emacs.app/Contents/MacOS/Emacs -nw $argv; 
+end
