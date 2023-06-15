0ce08723bed3fc2e9aa7718e67cd41a4de57c583
Vedang Manerikar <ved.manerikar@gmail.com>
2023-06-15 22:39:41 +0530

    Add gnu-sed and coreutils to the PATH
    Make this explicit by adding to config.fish, instead of relying on
    fish_variables
    
    Commit the resulting changes to fish_variables as well

--- fish/config.fish
+++ fish/config.fish
@@ -7,5 +7,7 @@ zoxide init fish | source
 fish_add_path $HOME/.cargo/bin
 fish_add_path /opt/homebrew/bin
 fish_add_path $HOME/src/prototypes/bb-scripts/bin
+fish_add_path /opt/homebrew/opt/coreutils/libexec/gnubin
+fish_add_path /opt/homebrew/opt/gnu-sed/libexec/gnubin
 
 direnv hook fish | source
